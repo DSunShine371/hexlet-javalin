e8aaefb7396d25db52ecf336fa14e152e159aec5
DSunShine371 <pestic077@gmail.com>
2025-06-24 10:56:08 +0300

    Initial project setup for HexletJavalin

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -7,7 +7,10 @@ public class HelloWorld {
         // Создаем приложение
         var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
